e350191edbe4aa57fe8ded37bdc369cee6924029
dassoumyacareers <121868704+dassoumyacareers@users.noreply.github.com>
2023-01-03 18:54:22 +0530

    revised salary

---
+++ example/sal_hike.py
@@ -0,0 +1,8 @@
+# giving a raise to the employee
+user_sal=int(input("enter your salary : "))
+final_sal=user_sal*120/100
+emp_name='Deepak'
+print("%d is the resulted salary",final_sal)
+print(" the resulted salary is {}".format(final_sal))
+print(f"the resulted salary is {final_sal} congrats!! {emp_name}")
+print("%d is the resulted salary" % final_sal)
