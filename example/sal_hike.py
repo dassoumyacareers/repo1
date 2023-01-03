1b5aad9f91481eeab06b480d4556cd8dc83cbe53
dassoumyacareers <121868704+dassoumyacareers@users.noreply.github.com>
2023-01-03 18:57:42 +0530

    correction to the existing code

--- example/sal_hike.py
+++ example/sal_hike.py
@@ -2,7 +2,7 @@
 user_sal=int(input("enter your salary : "))
 final_sal=user_sal*120/100
 emp_name='Deepak'
-print("%d is the resulted salary",final_sal)
+print("%d is the resulted salary"%final_sal)
 print(" the resulted salary is {}".format(final_sal))
 print(f"the resulted salary is {final_sal} congrats!! {emp_name}")
 print("%d is the resulted salary" % final_sal)
